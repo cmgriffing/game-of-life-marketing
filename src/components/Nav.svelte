5d9bc5db7c4550b1fa9180b896ffe2bc14329256
Chris Griffing <cmgriffing@gmail.com>
2020-06-19 11:01:17 -0700

    Initial commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { Button } from 'svelma';
+
+
+	export function isSegmentActive(_segment, thisSegment) {
+		if((!_segment && !thisSegment) || _segment === thisSegment) {
+			return 'page';
+		}
+	}
+
+	export let segment;
+</script>
+
+<style>
+ul {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+}
+
+a[aria-current="page"] {
+	text-decoration: underline;
+}
+</style>
+
+<nav>
+	<ul>
+		<li><a aria-current="{isSegmentActive(segment)}" href=".">Home</a></li>
+		<li><a aria-current="{isSegmentActive(segment, 'scores')}" href="scores">High Scores</a></li>
+		<li class="spacer"></li>
+		<li><Button as="a" href="game">Play the Game!</Button></li>
+	</ul>
+</nav>
---
+++ src/routes/scores.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { onMount } from 'svelte';
+	import axios from 'axios';
+
+	let scores = []
+	onMount(async () => {
+		const result = await axios.get('http://localhost:3100/prod/get-high-scores');
+		console.log({result});
+		scores = result.data.scores;
+	});
+
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<table class="table is-fullwidth">
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Steps</th>
+			<th>Active</th>
+			<th>Preview</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each scores as score}
+			<tr>
+				<td></td>
+				<td>{score.step_count}</td>
+				<td>{score.active_count}</td>
+				<td></td>
+			</tr>
+		{/each}
+	</tbody>
+
+</table>
